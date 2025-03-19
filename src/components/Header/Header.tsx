035f5c6f4271c266c1b3a423d6abfdef8b6f3f15
Илья Копендох <ilakopendoh@MacBook-Air-Ila.local>
2025-03-19 14:17:30 +0300

    header logo fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,7 @@ const Header = () => (
       <HeaderSearch />
       <HeaderButtons />
       <HeaderLogo />
+      <button type="button" className="header__profile header__profile-mobile" />
       <HeaderBurger />
       <HeaderMenu />
       <a href="#sobes" className="header__button header__link">
--- src/components/Header/HeaderButtons.tsx
+++ src/components/Header/HeaderButtons.tsx
@@ -1,6 +1,5 @@
 export const HeaderButtons = () => (
 	<div className="header__buttons">
 	  <button type="button" className="header__cart" />
-	  <button type="button" className="header__profile" />
 	</div>
   );
